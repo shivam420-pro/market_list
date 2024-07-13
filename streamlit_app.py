495e5c813a88ce3b12d5b7132acc96ef7524e343
shivam sharma <78008879+shivam420-pro@users.noreply.github.com>
2024-07-13 20:32:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,33 @@
 import streamlit as st
+import pymongo
+import pandas as pd
 
-st.title("market recherche")
+
+
+
+def get_data_from_mongo():
+    # Replace with your MongoDB connection string
+    connection_string = "mongodb+srv://jadoo_shivam:211101@sharemarket.q0zep9q.mongodb.net/"
+    client = pymongo.MongoClient(connection_string)
+    
+    # Replace 'your_database' and 'your_collection' with actual database and collection names
+    db = client['jadoo_data']
+    collection = db['Data']
+    
+    # Fetch data from MongoDB collection into a DataFrame
+    cursor = collection.find({})
+    df = pd.DataFrame(list(cursor))
+    
+    return df
+def main():
+    st.title('MongoDB Data Viewer')
+    
+    # Connect to MongoDB and fetch data
+    df = get_data_from_mongo()
+    
+    # Display the DataFrame
+    st.write("### Data from MongoDB:")
+    st.write(df)
+
+if __name__ == "__main__":
+    main()
